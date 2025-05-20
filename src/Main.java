2ade61741ff0747b2d8b59840453c4ea0b1e0189
SergeyGriko <206880@gmail.com>
2025-05-20 23:27:41 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -18,26 +19,42 @@ public class Main {
 
         System.out.println();
         System.out.println("Задача Третья");
-        for (int index = intArray.length - 1; index >= 0; index-- ) {
+        for (int index = intArray.length - 1; index >= 0; index--) {
             if (index == 0) {
                 System.out.println(intArray[index]);
                 break;
             }
             System.out.print(intArray[index] + ", ");
         }
-        for (int index = doubleArray.length - 1; index >= 0; index-- ) {
+        for (int index = doubleArray.length - 1; index >= 0; index--) {
             if (index == 0) {
                 System.out.println(doubleArray[index]);
                 break;
             }
             System.out.print(doubleArray[index] + ", ");
         }
-        for (int index = stringArray.length - 1; index >= 0; index-- ) {
+        for (int index = stringArray.length - 1; index >= 0; index--) {
             if (index == 0) {
                 System.out.println(stringArray[index]);
                 break;
             }
             System.out.print(stringArray[index] + ", ");
         }
+
+        System.out.println();
+        System.out.println("Задача Четвертая");
+        for (int index = 0; index <= intArray.length - 1; index++) {
+            if (intArray[index] % 2 != 0) {
+                intArray[index] += 1;
+            }
+
+        }
+        System.out.println(Arrays.toString(intArray));
+
+
+        System.out.println();
+        System.out.println("Задача Четвертая");
+
     }
+
 }
