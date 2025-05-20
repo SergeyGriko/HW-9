85b6a5679ca5200af10bf62a2533e1abd86eb1de
SergeyGriko <206880@gmail.com>
2025-05-20 23:01:23 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,43 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Задача Первая");
+        int[] intArray = new int[3];
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+        double[] doubleArray = {1.57, 7.654, 9.986};
+        String[] stringArray = {"яблоко", "банан", "вишня"};
+
+        System.out.println();
+        System.out.println("Задача Вторая");
+        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(doubleArray));
+        System.out.println(Arrays.toString(stringArray));
+
+        System.out.println();
+        System.out.println("Задача Третья");
+        for (int index = intArray.length - 1; index >= 0; index-- ) {
+            if (index == 0) {
+                System.out.println(intArray[index]);
+                break;
+            }
+            System.out.print(intArray[index] + ", ");
+        }
+        for (int index = doubleArray.length - 1; index >= 0; index-- ) {
+            if (index == 0) {
+                System.out.println(doubleArray[index]);
+                break;
+            }
+            System.out.print(doubleArray[index] + ", ");
+        }
+        for (int index = stringArray.length - 1; index >= 0; index-- ) {
+            if (index == 0) {
+                System.out.println(stringArray[index]);
+                break;
+            }
+            System.out.print(stringArray[index] + ", ");
+        }
     }
 }
